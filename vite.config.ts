eeb36ca77069deb53d860f2cc67a95082e75789b
Gowri Vadmal <73438758+mudkins@users.noreply.github.com>
2026-06-29 21:42:30 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import { nitro } from 'nitro/vite'; // <-- Add this line
+import { nitro } from 'nitro/vite';
+import tailwindcss from '@tailwindcss/vite'; // <-- 1. Ensure this is imported
 import viteReact from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    tailwindcss(), // <-- 2. Place this right at the top of the list!
     tanstackStart(), 
-    nitro(), // <-- Add this line instead of the netlify() plugin
+    nitro(), 
     viteReact(), 
     tsconfigPaths()
   ],
