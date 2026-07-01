bae8fdfe99ecf4a7b3a6cf94ca81bb518d0758dc
Gowri Vadmal <73438758+mudkins@users.noreply.github.com>
2026-07-01 14:01:53 -0700

    Add files via upload

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,10 @@
-import { HomePage } from '../components/FoundationLayout'
 import { createFileRoute } from '@tanstack/react-router'
+import { HomePage } from '../FoundationLayout' // or your layout file path
 
 export const Route = createFileRoute('/')({
+  meta: () => [
+    { title: 'The Charles and Patricia Heidelberger Foundation for Cancer Research' },
+    { name: 'description', content: 'Honoring the legacy of Dr. Charles Heidelberger by supporting cancer research fellowships, international symposia, and global scientific exchange.' },
+  ],
   component: HomePage,
 })
