b293ac05255ea2678bfc2eed47270d8335fe9398
Gowri Vadmal <73438758+mudkins@users.noreply.github.com>
2026-07-01 14:02:59 -0700

    Add files via upload

--- src/routes/mission.tsx
+++ src/routes/mission.tsx
@@ -2,9 +2,5 @@ import { createFileRoute } from '@tanstack/react-router'
 import { MissionPage } from '../components/FoundationLayout'
 
 export const Route = createFileRoute('/mission')({
-  meta: () => [
-    { title: 'Our Mission | Heidelberger Foundation' },
-    { name: 'description', content: 'Nurturing the next generation of cancer researchers and fostering innovative collaborations in the global effort against cancer.' },
-  ],
   component: MissionPage,
 })
